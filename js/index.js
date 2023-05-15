f46986db05da9142b2439d1f334dcbc2becb9832
hermtechs <hermtechs@gmail.com>
2023-05-15 04:03:16 +0300

    functionality of order through whatsapp

--- js/index.js
+++ js/index.js
@@ -2,6 +2,6 @@ const orderButton = document.querySelectorAll(".order-product");
 document.addEventListener("DOMContentLoaded", documentReady);
 function documentReady() {
   orderButton.forEach((button) => {
-    button.href = `https://wa.me/97470923424?text=https://I'm%20interested%20in%20your%20car%20for%20sale`;
+    button.href = `https://wa.me/97470923424?text=https://I'm%20interested%20in%20your%20car%20%https://poshboutiqueug.com/product/tshirt%20for%20sale`;
   });
 }
