9e4c8bc6b81a2c76d912086df9aebd19a6d0f286
hermtechs <hermtechs@gmail.com>
2023-05-15 03:51:51 +0300

    added a contact me btn

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const orderButton = document.querySelectorAll(".order-product");
+document.addEventListener("DOMContentLoaded", documentReady);
+function documentReady() {
+  orderButton.forEach((button) => {
+    button.href = `https://wa.me/97470923424text=I'm%20interested%20in%20your%20car%20for%20sale`;
+  });
+}
