5eadac1ed4877c085eadc8f8cd45c861f045a7df
hermtechs <hermtechs@gmail.com>
2023-05-15 05:11:55 +0300

    _

--- js/index.js
+++ js/index.js
@@ -4,6 +4,6 @@ function documentReady() {
   orderButton.forEach((button) => {
     // button.href = `https://wa.me/97470923424?text=I'm%20interested%20in%20your%20car%20%sale20%https://poshboutiqueug.netlify.app/product/`;
     button.href =
-      "https://wa.me/974682452378?text=sjgd%20zdz%20jdbmabz%20https://web.com/#hello";
+      "https://wa.me/97470923424?text=hello%20I%20got%20this%20hello%20https://hello.netlify.app/#hello";
   });
 }
