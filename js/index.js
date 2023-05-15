b185453a2a891e201bb685af5cb1e279e17bb8ca
hermtechs <hermtechs@gmail.com>
2023-05-15 05:09:23 +0300

    _

--- js/index.js
+++ js/index.js
@@ -2,6 +2,8 @@ const orderButton = document.querySelectorAll(".order-product");
 document.addEventListener("DOMContentLoaded", documentReady);
 function documentReady() {
   orderButton.forEach((button) => {
-    button.href = `https://wa.me/97470923424?text=I'm%20interested%20in%20your%20car%20%sale20%https://poshboutiqueug.netlify.app/product/`;
+    // button.href = `https://wa.me/97470923424?text=I'm%20interested%20in%20your%20car%20%sale20%https://poshboutiqueug.netlify.app/product/`;
+    button.href =
+      "https://wa.me/974682452378?text=sjgd%20zdz%20jdbmabz%20https://web.com/#hello";
   });
 }
