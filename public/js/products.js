328719d0029543f1c7b5f8ac866a2a2a99f3801f
hermtechs <hermtechs@gmail.com>
2023-05-19 02:51:27 +0300

    added product card functionality by fetching dynamic content from contentful REST AOI

---
+++ public/js/products.js
@@ -0,0 +1,46 @@
+const client = contentful.createClient({
+  space: "74qhpdisy2mq",
+  environment: "master", // defaults to 'master' if not set
+  accessToken: "dwhouqiLZE7TM9bjII1u52irpIOZolnWiBk59dN8kmQ",
+});
+
+const productPhotoElement = document.querySelector(".product-photo");
+const productPriceElement = document.querySelector(".price-value");
+const orderBtnElement = document.querySelector(".product-photo");
+const productNameElement = document.querySelector(".product-name");
+
+const selectedProductId = "EcnvJFc7rInJCRniYFcmD";
+document.addEventListener("DOMContentLoaded", () => {
+  getProducts();
+});
+const getProducts = async () => {
+  await client.getEntries().then((res) => {
+    const allProducts = res.items;
+    const selectedProduct = allProducts.find((prod) => {
+      return prod.sys.id === selectedProductId;
+    });
+    // console.log(selectedProduct.fields.productName);
+    const { productName, productCurrentPrice, productPhoto } =
+      selectedProduct.fields;
+    const { url } = productPhoto.fields.file;
+    const imageUrl = `https:${url}`;
+    updateProductDOM(
+      productName,
+      imageUrl,
+      productCurrentPrice,
+      selectedProductId
+    );
+  });
+};
+
+const updateProductDOM = (
+  productName,
+  imageUrl,
+  productCurrentPrice,
+  selectedProductId
+) => {
+  productPhotoElement.src = imageUrl;
+  productPriceElement.innerText = productCurrentPrice;
+  productNameElement.innerText = productName;
+  orderBtnElement.href = `https://${selectedProductId}`;
+};
